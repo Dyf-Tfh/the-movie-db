10fb8b19080d7ab06cb9a77903064fcc65c414bf
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-01 14:51:37 +0200

    index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.create(title: "Fate/Kaleid Liner Prisma Illya: Sekka no Chikai", release_year: 2017, director: "Oonuma Shin")
+Movie.create(title:"Fate/Stay Night: Heaven`s Feel", release_year: 2017, director: "Sudou Tomonori")
+Movie.create(title:"No Game no Life Zero", release_year: 2017, director: "Ishizuka Atsuko")
